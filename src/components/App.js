8d521568cc64c46ae9ee6590937bfe8fe71939ad
Colin Maher <colinmaher94@gmail.com>
2021-01-28 22:49:43 +0100

    Improve buttons and add copy on click to email

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import { useState } from "react"
-import { createMuiTheme, Container, Typography, Card, CardContent, Button, CssBaseline, Paper, Grid, Link, Tooltip, IconButton } from "@material-ui/core"
+import { createMuiTheme, Container, Typography, Card, CardContent, Button, CssBaseline, Paper, Grid, Link, Tooltip, IconButton, ClickAwayListener } from "@material-ui/core"
 import { ThemeProvider, makeStyles } from "@material-ui/core/styles"
 import Header from "components/Header"
 import Footer from "components/Footer"
 import "styles/style.css"
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import AssignmentIcon from '@material-ui/icons/Assignment'
+import "typeface-dosis"
+import { Autorenew } from "@material-ui/icons"
+import EmailIcon from '@material-ui/icons/Email';
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
-  const [spacing, setSpacing] = useState(3)
   
   const theme = createMuiTheme({
     spacing: 10,
@@ -25,6 +28,12 @@ function App() {
         main: darkMode ? "#000000" : "#ffffff"
       }
     },
+    typography: {
+      fontFamily: [
+        'dosis'
+      ],
+      fontSize: 15
+    },
     props: {
       MuiPaper: {
         elevation: 0,
@@ -32,7 +41,7 @@ function App() {
         
       },
       MuiButton: {
-        color: "primary"
+        
       }
     },
     overrides: {
@@ -45,75 +54,121 @@ function App() {
   const useStyles = makeStyles((theme) => ({
     content: {
       maxWidth: 1000,
-      padding: theme.spacing(spacing),
+      padding: "0px 20px",
       margin: "auto",
       marginTop: 0
     },
     paper: {
-      padding: theme.spacing(2),
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
+    title: {
+      fontSize: 40,
+      marginBottom: 10,
+      marginTop: 0
+    },
     button: {
-      width: 150
+      textTransform: "none",
+      padding: "15px",
+      margin: "auto",
+      justifyContent: "flex-start"
     }
   }))
 
   const classes = useStyles()
+  const [emailToolTipOpen, setEmailTooltipOpen] = useState(false)
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
       <div className={classes.content}>
-        <Grid container spacing={spacing}>
+        <Grid container spacing={1}>
           <Grid item xs={12} sm={6}>
-              <Typography variant="h3">Hey, I'm Colin :)</Typography>
-              <br/>
+              <Typography class={classes.title}>Hey, I'm Colin.</Typography>
               <Typography variant="body1">
                 I'm currently living in Cologne working as a freelance programming tutor, while I study German. 
                 I have a Bachelors of Science in Software Development. I am a massive nerd and I love everything tech. 
               </Typography>                  
           </Grid>
           <Grid item xs={0} md={6}/>
-          <Grid item xs={12}>
-            <Typography variant="h4">Projects</Typography>
-            <br/>
+          <Grid item xs={12} md={6}>
+            <Typography class={classes.title}>Projects</Typography>
             <Link href="https://github.com/ColinM94/homecare-ireland-web" color="inherit">
-              Homecare Ireland
+              <Typography>Homecare Ireland</Typography>
             </Link><br/>
             <Link href="https://github.com/ColinM94/divitiae" color="inherit">
-              Divitiae
+              <Typography>Divitiae</Typography>
             </Link>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h4">Skills and Tools</Typography>
-            <br/>
+          <Grid item xs={0} md={6}/>
+          <Grid item xs={12} md={6}>
+            <Typography class={classes.title}>Skills and Tools</Typography>
             <Typography variant="body1">
               JavaScript, React, React Native, C#, C++, Java. 
             </Typography>  
           </Grid>
+          <Grid item xs={0} md={6}/>
+          <Grid item xs={12} md={6}>
+            <Typography class={classes.title}>Tutoring</Typography>
+            <Typography variant="body1">
+              I can teach you general programming, or focus any specific areas you want, and/or assist you with your college assignments.
+              The lessons are online through Zoom and are €25/hr.
+            </Typography>
+          </Grid>
+          <Grid item xs={0} md={6}/>
           <Grid item xs={12}>
-            <Typography variant="h4">Contact Me</Typography>
+            <Typography class={classes.title}>Contact Me</Typography>
+            <Typography variant="body1" style={{marginBottom: 10}}>I would love to hear from you!</Typography>
+            <ClickAwayListener onClickAway={() => setEmailTooltipOpen(false)}>
+              <Tooltip 
+                title="Copied to Clipboard"
+                disableFocusListener
+                disableHoverListener
+                onClose={() => setEmailTooltipOpen(false)}
+                open={emailToolTipOpen}
+                onClick={() => setEmailTooltipOpen(true)}
+              >
+                <Button            
+                  startIcon={<EmailIcon />}
+                  className={classes.button}
+                  onClick={() => {navigator.clipboard.writeText("colinmaher94@gmail.com"); setEmailTooltipOpen(true)}}
+                >
+                  <Typography>
+                    ColinMaher94@gmail.com
+                  </Typography>
+                </Button>
+              </Tooltip>
+            </ClickAwayListener>
             <br/>
-            <Typography variant="body1">colimaher94@gmail.com</Typography>
-            <Button 
-              variant="outlined"
-              size="large"
+            <Button            
               startIcon={<GitHubIcon />}
               className={classes.button}
               onClick={() => window.open("https://github.com/ColinM94")}
             >
-              GitHub
+              <Typography>
+                GitHub
+              </Typography>
             </Button>
-            <Button 
-              variant="outlined"
-              size="large"
+            <br/>
+            <Button     
               startIcon={<LinkedInIcon />}
               className={classes.button}
               onClick={() => window.open("https://linkedin.com/in/colinm94")}
             >
-              LinkedIn
+              <Typography>
+                LinkedIn
+              </Typography>
+            </Button>
+            <br/>
+            <Button 
+              startIcon={<AssignmentIcon />}
+              className={classes.button}
+              onClick={()=> window.open("cv.pdf", "_blank")}
+            >
+              <Typography>
+                CV
+              </Typography>
             </Button>
           </Grid>
         </Grid>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Typography, Container, Button, Paper, makeStyles, Tooltip, IconButton } from "@material-ui/core"
 import Brightness7Icon from '@material-ui/icons/Brightness7'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+
 
 const useStyles = makeStyles((theme) => ({
     header: {
@@ -17,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
         paddingRight: theme.spacing(2)
     },
     button: {
-        width: 100
+        minWidth: 100
     },
     darkModeBtn: {
         marginLeft: "auto"
@@ -29,12 +32,13 @@ function Header(props) {
 
     return (
         <div className={classes.container}>
-            <Button className={classes.button} color="primary">Projects</Button>
-            <Button className={classes.button} color="primary">Contact</Button>
-            <Button className={classes.button} color="primary" onClick={()=> window.open("cv.pdf", "_blank")}>CV</Button>
+            <Button className={classes.button}>Projects</Button>
+            <Button className={classes.button}>Contact</Button>
+            <Button className={classes.button} onClick={()=> window.open("cv.pdf", "_blank")}>CV</Button>
+
             <Tooltip title={props.darkMode ? "Light Mode" : "Dark Mode"} className={classes.darkModeBtn} onClick={() => props.setDarkMode(!props.darkMode)}>
                 <IconButton aria-label="dark mode">
-                    <Brightness7Icon color="primary"/>
+                    <Brightness7Icon/>
                 </IconButton>
             </Tooltip>
         </div>    
